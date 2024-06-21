d25356b58d14677360d95da7047994978fd78e21
Hiroki-Imiya <yo4ro6si4ku9na7@gmail.com>
2024-06-21 15:10:10 +0900

    卒研用のjava,html,javascriptファイルの作成

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-
 //開発支援プログラムのメイン部分
 
 //ファイルからjavaのコードを読み取る
-// Read the file contents
-fs.readFile('/tmp.java', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
 
-    // Process the Java code
-    // You can perform any operations on the code here
-    console.log(data);
+var form=document.forms.myform;
+
+form.myfile.addEventListener("change",function(e){
+    //読み込んだファイル情報を取得
+    var fileData=e.target.files[0];
+    var reader=new FileReader();
+    //ファイル読み込み完了後の処理
+    reader.onload=function(){
+        //読み込んだデータを表示
+        form.myarea.value=reader.result;
+    }
 });
